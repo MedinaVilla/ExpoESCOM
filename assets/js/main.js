ba4625361a65251ffc71390b9a290ed51d412adb
jaimechacon11 <chacon.jaime99g@gmail.com>
2020-06-02 10:26:57 -0500

    telefono

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Squadfree - v2.0.0
-* Template URL: https://bootstrapmade.com/squadfree-free-bootstrap-template-creative/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
